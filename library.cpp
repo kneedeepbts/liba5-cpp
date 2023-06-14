20125a415221dc748c2107d057d9f686f0fab4dd
Daniel Williams <dwilliams@port8080.net>
2023-06-14 16:36:58 -0600

    Adding some initial files.

---
+++ library.cpp
@@ -0,0 +1,7 @@
+#include "library.h"
+
+#include <iostream>
+
+void A53_GSM(uint8_t *key, uint32_t klen, uint32_t count, uint8_t *block1, uint8_t *block2 ); {
+    // Stuff here...
+}
---
+++ library.h
@@ -0,0 +1,8 @@
+#ifndef LIBA5_CPP_LIBRARY_H
+#define LIBA5_CPP_LIBRARY_H
+
+#include <cstdint>
+
+void A53_GSM(uint8_t *key, uint32_t klen, uint32_t count, uint8_t *block1, uint8_t *block2 );
+
+#endif //LIBA5_CPP_LIBRARY_H
